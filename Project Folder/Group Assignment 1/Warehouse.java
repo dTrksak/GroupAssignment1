b032bbce2315a61b0bbbdda1983662cd3cf9f955
jCodeMonkey <joseph.hall@metrostate.edu>
2020-01-31 20:31:58 -0600

    warehouse bones

--- Project Folder/Group Assignment 1/Warehouse.java
+++ Project Folder/Group Assignment 1/Warehouse.java
@@ -1,5 +1,29 @@
+import java.util.*;
 
-public class Warehouse
-{
+public class Warehouse {
+	
+private String warehouseID;
+private List<Shipment> shipList = new ArrayList<Shipment>();
+private boolean availability = true;
 
+public Warehouse(String wareID){
+	warehouseID = wareID;
+}
+public void enableFreightReceipt(){
+	availability = true;
+}
+public void disableFrieghtReceipt(){
+	availablity = false;
+}
+public boolean  getAvailability(){
+	return availability;
+}
+public void addShipment(Shipment shipment){
+	if(availability == true){
+	shipList.add(shipment);
+	}else{
+		System.out.println("Sorry that warehouse is accepting shipments");
+	}
+}
+}
 }
