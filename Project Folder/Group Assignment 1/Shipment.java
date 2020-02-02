118b243f9d8ca07f3df861f24bcb6748b84c0a17
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-02 12:12:14 -0600

    Update Shipment.java

--- Project Folder/Group Assignment 1/Shipment.java
+++ Project Folder/Group Assignment 1/Shipment.java
@@ -1,4 +1,38 @@
-
-public class Shipment {
-
+public class Shipment
+{
+	String warehouseID;
+	String shipmentID;
+	String shipmentMethod;
+	float weight;
+	long receiptDate;
+	
+	public Shipment(String warehouseID, String shipmentID, String shipmentMethod, float weight, long receiptDate)
+	{
+		this.warehouseID = warehouseID;
+		this.shipmentID = shipmentID;
+		this.shipmentMethod = shipmentMethod;
+		this.weight = weight;
+		this.receiptDate = receiptDate;
+	}
+	
+	public String getWarehouseID()
+	{
+		return this.warehouseID;
+	}
+	public String getShipmentID()
+	{
+		return this.shipmentID;
+	}
+	public String getShipmentMethod()
+	{
+		return this.shipmentMethod;
+	}
+	public float getWeight()
+	{
+		return this.weight;
+	}
+	public long receiptDate()
+	{
+		return this.receiptDate;
+	}
 }
