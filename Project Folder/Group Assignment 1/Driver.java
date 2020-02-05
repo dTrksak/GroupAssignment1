5386a2ae50416a76eb4e050ca855f00ad3f8889a
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-04 19:21:50 -0600

    Update Driver.java

--- Project Folder/Group Assignment 1/Driver.java
+++ Project Folder/Group Assignment 1/Driver.java
@@ -3,6 +3,8 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		
+		WarehouseHandler wHandler = new WarehouseHandler();
+		JsonHandler jHandler = new JsonHandler();
+		InputHandler iHandler = new InputHandler(wHandler, jHandler);
 	}
 }
