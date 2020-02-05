47b45cff662d89ca856dc55c999ab3dadaaec0de
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-05 16:08:49 -0600

    Update FileOperations.java

--- Project Folder/Group Assignment 1/FileOperations.java
+++ Project Folder/Group Assignment 1/FileOperations.java
@@ -35,13 +35,32 @@ public class FileOperations
 	    }
     }
 	
+	public String fileDirectory()
+	{
+		JFileChooser fileChooser = new JFileChooser(); //open the file chooser
+    	JDialog dialog = new JDialog();  
+
+	    fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir"))); //set the current directory to be the one where this program is in
+	    int result = fileChooser.showOpenDialog(dialog);
+	    if (result == JFileChooser.APPROVE_OPTION) //if a file is approved
+	    {
+	    	return System.getProperty("user.dir");
+	    }
+	    else //if the file chooser is cancelled
+	    {
+	        System.out.println("Program canceled.");
+	        System.exit(0); //exit the program
+	        return null;
+	    }
+	}
+	
 	//creates a new file from an inputed file
 	public File createFile(String directory, String fileName) throws IOException //creates a file in the file directory
     {
     	File f = new File(directory + "\\"+fileName+".txt"); //needed to add double slashes to the directory for it to work correctly
 		if (!f.createNewFile())
 		{
-		    System.out.println("ERROR: outputFile already in folder, cannot create duplicate. Delete outputFile and restart the program."); //smaller file error handling block
+		    System.out.println("ERROR: outputFile already in folder, cannot create duplicate. Delete "+fileName+" and restart the program."); //smaller file error handling block
 		    System.out.println("Program unsuccessful."); //tell the user the program failed, and they must delete the other outputFile (I could have used a for loop to create outputFile2...n, but I don't want file spam)
 		    System.exit(0); //quit the program
 		}
@@ -67,17 +86,4 @@ public class FileOperations
 	    }
 	    return contentBuilder.toString();
 	}
-	
-	//converts an array-list to an integer array
-    public static int[] convertIntegers(List<Integer> integers) //This is from: https://stackoverflow.com/questions/718554/how-to-convert-an-arraylist-containing-integers-to-primitive-int-array
-    {
-        int[] ret = new int[integers.size()];
-        Iterator<Integer> iterator = integers.iterator();
-        for (int i = 0; i < ret.length; i++)
-        {
-            ret[i] = iterator.next().intValue();
-        }
-        return ret;
-    }
 }
-
