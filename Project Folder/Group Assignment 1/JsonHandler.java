6b6e8155179a5954dced20d924b4bfe5f88eee51
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-07 16:22:11 -0600

    Update JsonHandler.java

--- Project Folder/Group Assignment 1/JsonHandler.java
+++ Project Folder/Group Assignment 1/JsonHandler.java
@@ -1,8 +1,6 @@
 import java.util.*;
 import com.google.gson.*;
 import java.io.*;
-import java.lang.reflect.Type;
-import com.google.gson.reflect.TypeToken;
 
 
 public class JsonHandler{
@@ -19,26 +17,50 @@ public class JsonHandler{
 		
         List<Shipment> shipList = new ArrayList<>();
         
-        JsonElement jElem = gson.fromJson(shipJson, JsonElement.class);
-        JsonObject shipObject = jElem.getAsJsonObject();
-        JsonArray shipArray = shipObject.getAsJsonArray("warehouse_contents");
+        JsonArray shipArray = new JsonArray();
+        try {
+        	JsonElement jElem = gson.fromJson(shipJson, JsonElement.class);
+        	JsonObject shipObject = jElem.getAsJsonObject();
+            shipArray = shipObject.getAsJsonArray("warehouse_contents");
+        } catch(JsonSyntaxException e) {
+        	System.out.println("Json syntax exception in file, cannot add shipments.");
+        	return null; //break out of the function
+        }
         
-		for(JsonElement i : shipArray) {
-			JsonObject shipmentObj = i.getAsJsonObject();
-			String warehouseID = shipmentObj.get("warehouse_id").getAsString();
-			String shipmentMethod = shipmentObj.get("shipment_method").getAsString();
-			String shipmentID = shipmentObj.get("shipment_id").getAsString();
-			float weight = shipmentObj.get("weight").getAsFloat();
-			long receiptDate = shipmentObj.get("receipt_date").getAsLong();
-			
-			if(h.getWarehouse(warehouseID) == null)
-			{
+        try {
+			for(JsonElement i : shipArray) {
+				JsonObject shipmentObj = i.getAsJsonObject();
+				String warehouseID = shipmentObj.get("warehouse_id").getAsString();
+				String shipmentMethod = shipmentObj.get("shipment_method").getAsString();
+				String shipmentID = shipmentObj.get("shipment_id").getAsString();
+				//Check that weight and receipt date are correctly formatted
+				try {
+					Float.parseFloat(shipmentObj.get("weight").getAsString());
+					Long.parseLong(shipmentObj.get("receipt_date").getAsString());
+				} catch(NumberFormatException e)
+				{
+					System.out.println("Number format exception, unable to add shipment "+shipmentID+". A float and/or a long are not correctly formatted.");
+					continue; //continue the for loop
+				}
+				float weight = shipmentObj.get("weight").getAsFloat();
+				long receiptDate = shipmentObj.get("receipt_date").getAsLong();
+				
+				//If an id contains a comma, do not add that shipment
+				if(warehouseID.contains(",") || shipmentID.contains(",")) {
+					System.out.println("Value error, unable to add shipment "+shipmentID+". An ID contained a comma.");
+					continue;
+				}
+				//Everything is good, create the shipment and the warehouse if needed
 				h.addWarehouse(warehouseID);
+				Shipment s = h.addShipment(warehouseID,shipmentMethod,shipmentID,weight,receiptDate);
+				shipList.add(s);
 			}
-			Shipment s = h.addShipment(warehouseID,shipmentMethod,shipmentID,weight,receiptDate);
-			shipList.add(s);
-		}
-		return shipList;
+			System.out.println("Shipments successfully imported.");
+        } catch(NullPointerException e) {
+        	System.out.println("Shipment element mislabled, please check the file for typos.");
+        	System.out.println("Could not import the rest of the shipments.");
+        }
+        return shipList;
     }
 	
 	public void shipmentToJson(ArrayList<Shipment> list) throws IOException {
@@ -46,15 +68,22 @@ public class JsonHandler{
 		FileOperations fo = new FileOperations();
 		File directory = fo.fileDirectory();
 		
+		if(directory == null) {
+			System.out.println("Export cancelled.");
+			return; //If the user cancelled the export, return
+		}
+		
 		WarehouseContents contents = new WarehouseContents(list);
 		Gson gson = new GsonBuilder().setPrettyPrinting().create(); //Output with pretty indentation
 		String json = gson.toJson(contents);
 		
 		File outFile = fo.createFile(directory.getPath(), "outputFile");
 		
-		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
-		writer.write(json);
-	    writer.close();
-	    
+		if(outFile != null) {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+			writer.write(json);
+		    writer.close();
+		    System.out.println("Shipments successfully exported.");
+		}
 	}
 }
