a9a92e0c17c5ed5836f7f8518281071afbd5b002
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-07 13:36:28 -0600

    Add files via upload

---
+++ Project Folder/Group Assignment 1/WarehouseContents.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class WarehouseContents
+{
+	List<Shipment> warehouse_shipments;
+	
+	public WarehouseContents(ArrayList<Shipment> list) {
+		this.warehouse_shipments = list;
+	}
+}
