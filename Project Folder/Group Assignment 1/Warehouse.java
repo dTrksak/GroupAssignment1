5939eaebdf16c4e0c512ba517e1b5e625dd10925
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-07 16:34:52 -0600

    Update Warehouse.java

--- Project Folder/Group Assignment 1/Warehouse.java
+++ Project Folder/Group Assignment 1/Warehouse.java
@@ -2,34 +2,36 @@ import java.util.*;
 
 public class Warehouse {
 	
-private String warehouseID;
-private List<Shipment> shipList = new ArrayList<Shipment>();
-private boolean availability = true;
-
-public Warehouse(String warehouseID){
-	this.warehouseID = warehouseID;
-}
-public void enableFreightReceipt(){
-	availability = true;
-}
-public void disableFrieghtReceipt(){
-	availability = false;
-}
-public boolean  getAvailability(){
-	return availability;
-}
-public void addShipment(Shipment shipment){
-	if(availability == true){
-	shipList.add(shipment);
-	}else{
-		System.out.println("Sorry that warehouse is not accepting shipments");
+	private String warehouseID;
+	private List<Shipment> shipList = new ArrayList<Shipment>();
+	private boolean availability = true;
+	
+	public Warehouse(String wareID){
+		warehouseID = wareID;
 	}
-}
-public String getWarehouseID(){
-	return warehouseID;
-}
-public List<Shipment> getShipmentList()
-{
-	return shipList;
-}
+	public void enableFreightReceipt(){
+		availability = true;
+	}
+	public void disableFreightReceipt(){
+		availability = false;
+	}
+	public boolean  getAvailability(){
+		return availability;
+	}
+	public Shipment addShipment(Shipment shipment){
+		if(availability == true){
+			shipList.add(shipment);
+			return shipment;
+		}else{
+			System.out.println("Sorry that warehouse is not accepting shipments");
+			return null;
+		}
+	}
+	public String getWarehouseID(){
+		return warehouseID;
+	}
+	public List<Shipment> getShipmentList()
+	{
+		return shipList;
+	}
 }
