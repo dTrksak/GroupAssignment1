dbcded4735b2169f0b6ea2539bbf14622cb1b3c2
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-09 12:45:52 -0600

    Update WarehouseContents.java
    Added comments

--- Project Folder/Group Assignment 1/WarehouseContents.java
+++ Project Folder/Group Assignment 1/WarehouseContents.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
+//Used to get to a list inside of a Json object
 public class WarehouseContents
 {
 	List<Shipment> warehouse_contents;
 	
-	public WarehouseContents(ArrayList<Shipment> list) {
+	public WarehouseContents(ArrayList<Shipment> list)
+	{
 		this.warehouse_contents = list;
 	}
 }
