43347700cd24af6268b94d8ba8a8c077a9c277d7
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-10 10:57:05 -0600

    Update Driver.java
    Fixed unnecessary references to warehousehandler and jsonhandler

--- Project Folder/Group Assignment 1/Driver.java
+++ Project Folder/Group Assignment 1/Driver.java
@@ -3,9 +3,7 @@ public class Driver
 {
 	public static void main(String[] args) throws IOException
 	{
-		WarehouseHandler wHandler = new WarehouseHandler();
-		JsonHandler jHandler = new JsonHandler(wHandler);
-		InputHandler iHandler = new InputHandler(wHandler, jHandler);
+		InputHandler iHandler = new InputHandler();
 		iHandler.getInput();
 	}
 }
