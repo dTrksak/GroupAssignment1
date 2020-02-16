2949752aee20e81bfa9a0a0b7534800efdb58a44
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-02-15 19:41:52 -0600

    Update Warehouse.java
    Changed style

--- Project Folder/Group Assignment 1/Java Files/Warehouse.java
+++ Project Folder/Group Assignment 1/Java Files/Warehouse.java
@@ -1,33 +1,42 @@
 import java.util.*;
 
-public class Warehouse {
-	
+public class Warehouse 
+{
 	private String warehouseID;
 	private List<Shipment> shipList = new ArrayList<Shipment>();
 	private boolean availability = true;
 	
-	public Warehouse(String warehouseID){
+	public Warehouse(String warehouseID)
+	{
 		this.warehouseID = warehouseID;
 	}
-	public void enableFreightReceipt(){
+	public void enableFreightReceipt()
+	{
 		availability = true;
 	}
-	public void disableFreightReceipt(){
+	public void disableFreightReceipt()
+	{
 		availability = false;
 	}
-	public boolean  getAvailability(){
+	public boolean  getAvailability()
+	{
 		return availability;
 	}
-	public Shipment addShipment(Shipment shipment){ 
-		if(availability == true){ // add shipment to warehouse
+	public Shipment addShipment(Shipment shipment)
+	{ 
+		if(availability == true)
+		{ // add shipment to warehouse
 			shipList.add(shipment);
 			return shipment;
-		}else{ // warehouse is closed
+		}
+		else
+		{ // warehouse is closed
 			System.out.println("Sorry that warehouse is not accepting shipments");
 			return null;
 		}
 	}
-	public String getWarehouseID(){
+	public String getWarehouseID()
+	{
 		return warehouseID;
 	}
 	public List<Shipment> getShipmentList()
