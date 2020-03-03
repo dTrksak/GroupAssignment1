5bb836230efc129dc2f052e4e5dd549c7ffc121e
melnicarol <60165902+melnicarol@users.noreply.github.com>
2020-03-03 17:38:51 -0600

    Update Driver.java
    if any error gets thrown in program all inputted data gets saved before exiting

--- Project Folder/Group Assignment 1/Java Files/Driver.java
+++ Project Folder/Group Assignment 1/Java Files/Driver.java
@@ -4,10 +4,21 @@ public class Driver
 {
 	public static void main(String[] args) throws IOException
 	{
-
+		WarehouseHandler handle = new WarehouseHandler();
+		RecoverData reData = new RecoverData();
 		InputHandler iHandler = new InputHandler();
 		CompleteUI UI = new CompleteUI();
-		UI.launchUI();
+		
+		try {
+			iHandler.getInput();
+			UI.launchUI();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			ArrayList<Shipment> l = handle.getAllWarehouseShipments();
+			reData.saveData(l);
+		}
  
 	}
 }
