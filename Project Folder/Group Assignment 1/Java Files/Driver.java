fc94c3c57e274f2b8965ad9787b478074bd471da
melnicarol <60165902+melnicarol@users.noreply.github.com>
2020-03-03 17:57:35 -0600

    Update Driver.java

--- Project Folder/Group Assignment 1/Java Files/Driver.java
+++ Project Folder/Group Assignment 1/Java Files/Driver.java
@@ -4,21 +4,12 @@ public class Driver
 {
 	public static void main(String[] args) throws IOException
 	{
-		WarehouseHandler handle = new WarehouseHandler();
-		RecoverData reData = new RecoverData();
 		InputHandler iHandler = new InputHandler();
 		CompleteUI UI = new CompleteUI();
-		
-		try {
-			iHandler.getInput();
-			UI.launchUI();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			ArrayList<Shipment> l = handle.getAllWarehouseShipments();
-			reData.saveData(l);
-		}
+
+		iHandler.getInput();
+		UI.launchUI();
+
  
 	}
 }
