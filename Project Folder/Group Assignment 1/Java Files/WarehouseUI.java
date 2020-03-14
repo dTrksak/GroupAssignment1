c080c603c31cdbae1c526b526db2d27c9d3bedf6
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-03-13 20:07:42 -0500

    Update WarehouseUI.java

--- Project Folder/Group Assignment 1/Java Files/WarehouseUI.java
+++ Project Folder/Group Assignment 1/Java Files/WarehouseUI.java
@@ -15,6 +15,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,11 +32,10 @@ public class WarehouseUI extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	WarehouseHandler handle = new WarehouseHandler();
+	WarehouseHandler handle = WarehouseHandler.getInstance();
 	InputHandler Ihandle = new InputHandler();
 	CompleteUI text = new CompleteUI();
-	String S = "12513";
-	//Warehouse UIwarehouse =  handle.addWarehouse(S,"");
+	String S = null;
 
 	public void setupFrame() {
 
@@ -55,7 +55,7 @@ public class WarehouseUI extends JFrame {
 	 * Create the frame.
 	 */
 	public WarehouseUI() {
-		Warehouse warehouse =  handle.addWarehouse(S,"");
+		//Warehouse warehouse =  handle.addWarehouse(S,"");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 400);
 		contentPane = new JPanel();
@@ -65,11 +65,13 @@ public class WarehouseUI extends JFrame {
 		
 		JLabel lblToggleReciept = new JLabel("Toggle Reciept");
 		
+		JLabel lblAddWarehouse = new JLabel("Add Warehouse");
+		
 		JLabel lblAddShipment = new JLabel("Add Shipment");
 		
 		JLabel lblPrintShipments = new JLabel("Print Shipments");
 		
-		JLabel lblExport = new JLabel("Export Shipments");
+		JLabel lblExport = new JLabel("Export Warehouse Shipments");
 		
 		
 		JButton btnPrint = new JButton("Print");
@@ -79,9 +81,49 @@ public class WarehouseUI extends JFrame {
 				}
 			
 			});
-	
 		
-		JButton btnAdd = new JButton("Add");
+		JButton btnAddWarehouse = new JButton("Add Warehouse");
+		btnAddWarehouse.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				
+				JFrame frame = new JFrame();
+				frame.setSize(400, 300);
+				frame.setLayout(new FlowLayout());
+				JLabel addWarehouseID = new JLabel("Enter WarehouseID");
+				JTextField field = new JTextField(20);
+				frame.add(addWarehouseID);
+				frame.add(field);
+				JLabel addWarehouseName = new JLabel("Enter warehouse name");
+				JTextField field2 = new JTextField(20);
+				frame.add(addWarehouseName);
+				frame.add(field2);
+				
+				
+				frame.setVisible(true);
+				JButton doneBtn = new JButton("Done");
+				frame.add(doneBtn);
+				doneBtn.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						String wareHouseIDInit = field.getText();
+						String wareHouseNameInit = field2.getText();
+						
+						if(wareHouseIDInit.equals("") || wareHouseNameInit.equals("")) {
+							JOptionPane.showMessageDialog(null, "All fields should have a value.");
+						}
+						// User cannot create a warehouse here, they must use the create warehouse button.
+						else if(handle.getWarehouse(wareHouseIDInit) == null) {
+							
+							Ihandle.createWarehouseProcess(wareHouseIDInit, wareHouseNameInit);
+							frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); //Close the warehouse window
+						} else {
+							JOptionPane.showMessageDialog(null, "Warehouse "+wareHouseIDInit+" already exists.");
+						}
+					}
+				});
+			}
+		});
+		
+		JButton btnAdd = new JButton("Add Shipment");
 		btnAdd.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				
@@ -133,11 +175,36 @@ public class WarehouseUI extends JFrame {
 						String shipmentDateInit = field6.getText();
 						shipInfo[5] = shipmentDateInit;
 						
-						try {
-							Ihandle.createShipmentProcess(shipInfo);
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
+						if(wareHouseIDInit.equals("") || wareHouseNameInit.equals("") || wareHouseShipmentIDInit.equals("") ||
+								shipmentMethodInit.equals("") || wareHouseWeightInit.equals("") || shipmentDateInit.equals("")) {
+							JOptionPane.showMessageDialog(null, "All fields should have a value.");
+						}
+						// User cannot create a warehouse here, they must use the create warehouse button.
+						else if(handle.getWarehouse(wareHouseIDInit) != null) {
+							try {
+								Shipment s = Ihandle.createShipmentProcess(shipInfo);
+								if(s != null)
+								{
+									List<Shipment> saveList = new ArrayList<>();
+									saveList.add(s);
+									RecoverData.saveData(saveList);
+									JOptionPane.showMessageDialog(null, "Shipment added to warehouse "+handle.getWarehouse(wareHouseIDInit).getWarehouseID()+".");
+									frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); //Close the warehouse window
+								}
+								else if(handle.getWarehouse(wareHouseIDInit).getAvailability() == false) {
+									JOptionPane.showMessageDialog(null, "Warehouse "+handle.getWarehouse(wareHouseIDInit).getWarehouseID()+"'s freight receipt is disabled.");
+								}
+								else {
+									JOptionPane.showMessageDialog(null, "Could not add shipment to warehouse "+handle.getWarehouse(wareHouseIDInit).getWarehouseID()+". "
+											+ "Make sure the shipment information is correct. Also warehouse "+handle.getWarehouse(wareHouseIDInit).getWarehouseID()+
+											"'s name is '"+handle.getWarehouse(wareHouseIDInit).getWarehouseName()+"'.");
+								}
+							} catch (IOException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						} else {
+							JOptionPane.showMessageDialog(null, "Warehouse "+wareHouseIDInit+" doesn't exist.");
 						}
 					}
 				});
@@ -173,10 +240,21 @@ public class WarehouseUI extends JFrame {
 						String warehouseID = field.getText();
 						String status = field2.getText();
 						
-						if(status.equals("ON")) {
-							Ihandle.enableFreight(warehouseID);;
-						}else {
-							Ihandle.endFreight(warehouseID);
+						if(handle.getWarehouse(warehouseID) != null)
+						{
+							if(status.equalsIgnoreCase("ON")) {
+								Ihandle.enableFreight(warehouseID);
+								JOptionPane.showMessageDialog(null, "Warehouse "+handle.getWarehouse(warehouseID).getWarehouseID()+" freight receipt is enabled.");
+								frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); //Close the on/off window
+							} else if(status.equalsIgnoreCase("OFF")) {
+								Ihandle.endFreight(warehouseID);
+								JOptionPane.showMessageDialog(null, "Warehouse "+handle.getWarehouse(warehouseID).getWarehouseID()+" freight receipt is disabled.");
+								frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); //Close the on/off window
+							} else {
+								JOptionPane.showMessageDialog(null, "Please type ON or OFF"); //Don't close the window, if the user messed up the input
+							}
+						} else {
+							JOptionPane.showMessageDialog(null, "Warehouse "+warehouseID+" doesn't exist.");
 						}
 					}
 				});
@@ -188,11 +266,34 @@ public class WarehouseUI extends JFrame {
 		JButton btnExport = new JButton("Export");
 		btnExport.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				try {
-					Ihandle.exportAllWarehouse();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
+				JFrame frame = new JFrame();
+				frame.setSize(300, 200);
+				frame.setLayout(new FlowLayout());
+				JLabel addWarehouseID = new JLabel("Enter WarehouseID");
+				JTextField wField = new JTextField(20);
+				frame.add(addWarehouseID);
+				frame.add(wField);
+				
+				frame.setVisible(true);
+				JButton doneBtn = new JButton("Done");
+				frame.add(doneBtn);
+				doneBtn.addActionListener(new ActionListener(){
+					public void actionPerformed(ActionEvent e){
+						String warehouseID = wField.getText();
+						
+						try {
+							if(handle.getWarehouse(warehouseID) != null)
+							{
+								Ihandle.exportWarehouse(warehouseID);
+								frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+							} else {
+								JOptionPane.showMessageDialog(null, "Warehouse "+warehouseID+" doesn't exist.");
+							}
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+					}
+				});
 			}
 		});
 		
@@ -216,9 +317,14 @@ public class WarehouseUI extends JFrame {
 				doneBtn.addActionListener(new ActionListener(){
 					public void actionPerformed(ActionEvent e){
 
-						String name = Ihandle.getWarehouseName(field.getText());
-						
-						JOptionPane.showMessageDialog(null, name);
+						Warehouse w = handle.getWarehouse(field.getText());
+						if(w != null)
+						{
+							JOptionPane.showMessageDialog(null, "Warehouse "+w.getWarehouseID()+"'s name is "+w.getWarehouseName()+".");
+							frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+						} else {
+							JOptionPane.showMessageDialog(null, "Warehouse "+field.getText()+" does not exist.");
+						}
 					}
 				});
 
@@ -234,6 +340,7 @@ public class WarehouseUI extends JFrame {
 						.addComponent(lblGetWarehouseName)
 						.addComponent(lblToggleReciept)
 						.addComponent(lblPrintShipments)
+						.addComponent(lblAddWarehouse)
 						.addComponent(lblAddShipment)
 						.addComponent(lblExport))
 					.addPreferredGap(ComponentPlacement.RELATED, 162, Short.MAX_VALUE)
@@ -241,6 +348,7 @@ public class WarehouseUI extends JFrame {
 						.addComponent(getName)
 						.addComponent(tglbtnOnoff)
 						.addComponent(btnPrint)
+						.addComponent(btnAddWarehouse)
 						.addComponent(btnAdd)
 						.addComponent(btnExport))
 					.addContainerGap(32, Short.MAX_VALUE))
@@ -248,23 +356,27 @@ public class WarehouseUI extends JFrame {
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(26)
+					.addGap(23)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblGetWarehouseName)
 						.addComponent(getName))
-					.addGap(29)
+					.addGap(23)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblPrintShipments)
 						.addComponent(btnPrint))
 					.addGap(23)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblAddWarehouse)
+						.addComponent(btnAddWarehouse))
+					.addGap(23)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addComponent(lblAddShipment)
 						.addComponent(btnAdd))
-					.addGap(28)
+					.addGap(23) //22
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblToggleReciept)
 						.addComponent(tglbtnOnoff))
-					.addGap(37)
+					.addGap(23) //37
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 							.addComponent(lblExport)
 							.addComponent(btnExport))))
