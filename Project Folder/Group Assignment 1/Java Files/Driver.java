ca1268c850f0f3f415020a4b96ad74225f44b23e
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-03-13 20:06:24 -0500

    Update Driver.java

--- Project Folder/Group Assignment 1/Java Files/Driver.java
+++ Project Folder/Group Assignment 1/Java Files/Driver.java
@@ -7,10 +7,7 @@ public class Driver
 	public static void main(String[] args) throws IOException
 	{
 		RecoverData.oldData();
-		InputHandler iHandler = new InputHandler();
 		CompleteUI UI = new CompleteUI();
-
-		// iHandler.getInput();
 		UI.launchUI();
 	}
 }
