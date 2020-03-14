1597b07a5720a039240475627382aa5108b45f09
melnicarol <60165902+melnicarol@users.noreply.github.com>
2020-03-14 13:58:15 -0500

    Add files via upload

--- Project Folder/Group Assignment 1/Java Files/Driver.java
+++ Project Folder/Group Assignment 1/Java Files/Driver.java
@@ -6,8 +6,11 @@ public class Driver
 {
 	public static void main(String[] args) throws IOException
 	{
+	
 		RecoverData.oldData();
+		InputHandler IP	= new InputHandler();
+		IP.getInput();
 		CompleteUI UI = new CompleteUI();
-		UI.launchUI();
+		//UI.launchUI();
 	}
 }
--- Project Folder/Group Assignment 1/Java Files/RecoverData.java
+++ Project Folder/Group Assignment 1/Java Files/RecoverData.java
@@ -44,7 +44,6 @@ public class RecoverData
 			{
 				list.remove(s); //Get rid of the shipment
 			}
-
 		}
 		
 
