d01a691e7ba65f9fcc9f86773d2e39d047b958d3
melnicarol <60165902+melnicarol@users.noreply.github.com>
2020-04-24 20:38:27 -0500

    Update AddShipment.java

--- Android/java/edu/metrostate/ics372_assignment3/AddShipment.java
+++ Android/java/edu/metrostate/ics372_assignment3/AddShipment.java
@@ -1,9 +1,6 @@
 package edu.metrostate.ics372_androidstart_master;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
