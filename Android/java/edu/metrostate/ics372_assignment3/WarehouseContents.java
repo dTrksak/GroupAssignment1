c8393a46ad624d4df1c25fb7206d62bb3c762089
dTrksak <60165871+dTrksak@users.noreply.github.com>
2020-04-26 13:49:44 -0500

    Update WarehouseContents.java
    ArrayLists are outdated, Lists are in fashion now. This is the way.

--- Android/java/edu/metrostate/ics372_assignment3/WarehouseContents.java
+++ Android/java/edu/metrostate/ics372_assignment3/WarehouseContents.java
@@ -1,4 +1,4 @@
-package edu.metrostate.ics372_androidstart_master;
+package edu.metrostate.ics372_assignment3;
 import java.util.*;
 
 //Used to get to a list inside of a Json object
@@ -6,7 +6,7 @@ public class WarehouseContents
 {
 	List<Shipment> warehouse_contents;
 	
-	public WarehouseContents(ArrayList<Shipment> list)
+	public WarehouseContents(List<Shipment> list)
 	{
 		this.warehouse_contents = list;
 	}
